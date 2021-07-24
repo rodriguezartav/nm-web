313d4e91c2b1457007230ace1cf135a495e24335
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-07-23 19:24:16 -0600

    fc

--- components/Price.js
+++ components/Price.js
@@ -16,14 +16,14 @@ export default function Example(props) {
   const features = ["beds", "food", "bungalow", "mosquito", "kitchen", "price"];
 
   return (
-    <div className="bg-indigo-600">
+    <div className="bg-green-600">
       <div className="pt-12 sm:pt-16 lg:pt-20">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
           <div className="text-center">
             <h2 className="text-3xl text-white font-extrabold  sm:text-4xl lg:text-5xl">
               Pricing & Schedule
             </h2>
-            <p className="mt-4 text-xl text-indigo-100 max-w-2xl mx-auto">
+            <p className="mt-4 text-xl text-green-100 max-w-2xl mx-auto">
               Nature & Mind retreats are profitless, our financial goal is to
               break even at the end of each year while providing a fair price
               for everyone
@@ -33,7 +33,7 @@ export default function Example(props) {
       </div>
       <div className="mt-8 bg-white pb-16 sm:mt-12 sm:pb-20 lg:pb-28">
         <div className="relative">
-          <div className="absolute inset-0 h-1/2 bg-indigo-600" />
+          <div className="absolute inset-0 h-1/2 bg-green-600" />
           <div className="relative max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
             <div className="max-w-lg mx-auto rounded-lg shadow-lg overflow-hidden lg:max-w-none lg:flex">
               <div className="flex-1 bg-white px-6 py-8 lg:p-12">
@@ -47,7 +47,7 @@ export default function Example(props) {
                 </p>
                 <div className="mt-8">
                   <div className="flex items-center">
-                    <h4 className="flex-shrink-0 pr-4 bg-white text-sm tracking-wider font-semibold uppercase text-indigo-600">
+                    <h4 className="flex-shrink-0 pr-4 bg-white text-sm tracking-wider font-semibold uppercase text-green-600">
                       What`s included
                     </h4>
                     <div className="flex-1 border-t-2 border-gray-200" />
--- components/Retreat.js
+++ components/Retreat.js
@@ -1,5 +1,6 @@
 /* This example requires Tailwind CSS v2.0+ */
 import { InboxIcon, SparklesIcon } from "@heroicons/react/outline";
+import Image from "next/image";
 
 export default function Example() {
   return (
@@ -52,7 +53,9 @@ export default function Example() {
           </div>
           <div className="mt-12 sm:mt-16 lg:mt-0">
             <div className="pl-4 -mr-48 sm:pl-6 md:-mr-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-              <img
+              <Image
+                width="500"
+                height="600"
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:left-0 lg:h-full lg:w-auto lg:max-w-none"
                 src="https://tailwindui.com/img/component-images/inbox-app-screenshot-1.jpg"
                 alt="Inbox user interface"
@@ -109,7 +112,9 @@ export default function Example() {
           </div>
           <div className="mt-12 sm:mt-16 lg:mt-0 lg:col-start-1">
             <div className="pr-4 -ml-48 sm:pr-6 md:-ml-16 lg:px-0 lg:m-0 lg:relative lg:h-full">
-              <img
+              <Image
+                width="500"
+                height="600"
                 className="w-full rounded-xl shadow-xl ring-1 ring-black ring-opacity-5 lg:absolute lg:right-0 lg:h-full lg:w-auto lg:max-w-none"
                 src="https://tailwindui.com/img/component-images/inbox-app-screenshot-2.jpg"
                 alt="Customer profile user interface"
