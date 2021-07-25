fb55429850a097ddec8071d9040ddbb8111710ef
Roberto Rodriguez <roberto@rodriguezartavia.com>
2021-07-25 12:16:34 -0600

    set comming soon

--- components/Header.js
+++ components/Header.js
@@ -72,7 +72,7 @@ function Dropdown() {
         <>
           <div>
             <Menu.Button className="inline-flex justify-center w-full rounded-md shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-green-500">
-              EN
+              EN / ES
               <ChevronDownIcon
                 className="-mr-1 ml-2 h-5 w-5"
                 aria-hidden="true"
